c550f2cfcec191011909d9d6e0b8103a15433c1c
Ekaterina Nichiporenko <75068703+KatyaNichi@users.noreply.github.com>
2022-06-22 12:05:56 +0200

    fixed port

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get('/profile/:id', (req,res) => {handleProfileGet(req,res, db)})
 app.put('/image', (req,res) => {handleImage(req, res,db)})
 app.post('/imageurl', (req,res) => {handleApiCall(req, res)})
 
-app.listen(3000, () => {
-    console.log('app is runing on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is runing on port ${process.env.PORT}`)
 })
